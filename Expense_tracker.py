6095a444e0d462c14c23a0e3a0d45dadd705841d
Jsmi322 <Jsmi1467@students.kennesaw.edu>
2024-04-22 18:13:02 -0400

    Update Expense_tracker.py

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -2,11 +2,29 @@ def main():
     print(f"Running Expense Tracker!")
 
     # Get user input for expense.
+    get_user_expense()
+
 
     #Write their expense to a file.
+    save_expense_to_file()
+
 
     #Read file and summarize expenses.
-    pass
+    summarize_expenses()
+  
+
+def get_user_expense():
+    print(f"Getting User Expense")
+    input("Enter expense name: ")
+
+def save_expense_to_file():
+    print(f"Saving User Expense")
+   
+
+def summarize_expenses():
+    print(f"Summarizing User Expense")
+
+
 
 
 if __name__ == "__main__":
