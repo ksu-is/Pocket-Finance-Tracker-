941a7886f54c8582fcee357939824a806eb45586
Jsmi322 <Jsmi1467@students.kennesaw.edu>
2024-04-22 23:20:27 -0400

    Expenses.csv file created
    I have created this file to track the expenses as users input the data. It will keep all of the data in one place.

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -1,43 +1,66 @@
+from Expense import Expense
+
+
 def main():
     print(f"Running Expense Tracker!")
+    expense_file_path = "expenses.csv"
 
     # Get user input for expense.
-    get_user_expense()
+    expense = get_user_expense()
 
 
     #Write their expense to a file.
-    save_expense_to_file()
+    save_expense_to_file(expense, expense_file_path)
 
 
     #Read file and summarize expenses.
-    summarize_expenses()
+    summarize_expenses(expense_file_path)
   
 
 def get_user_expense():
     print(f"Getting User Expense")
     expense_name = input("Enter expense name: ")
     expense_amount = float(input("Enter expense amount: "))
-    print(f"You've entered {expense_name}, {expense_amount}")
-
-    expense_categories = ["🥞Food", "🏠Home", "💼Work", "🎊Fun", "✨Misc", "🩺Health"]
-
+    expense_categories = [
+        "🥞Food", 
+        "🏠Home", 
+        "💼Work", 
+        "🎊Fun", 
+        "✨Misc",
+        "🩺Health"
+    ]
 
     while True:
         print("Select a category: ")
         for i, category_name in enumerate(expense_categories):
             print(f" {i + 1}. {category_name}")
-        break
+
+        value_range = f"[1 - {len (expense_categories)}]"
+        selected_index = int(input(f"Enter a categpry number {value_range}: ")) - 1
+
+        if i in range(len(expense_categories)):
+            selected_category = expense_categories[selected_index]
+            new_expense = Expense(
+                name=expense_name, category=selected_category, amount=expense_amount
+            )
+            return new_expense
+        else:
+            print("Invalid Category. Please try again!")
+        
 
 
-def save_expense_to_file():
-    print(f"Saving User Expense")
+
+def save_expense_to_file(expense: Expense, expense_file_path):
+    print(f"Saving User Expense: {expense} to {expense_file_path}")
+    with open(expense_file_path, "a") as f:
+        f.write(f"{expense.name}, {expense.amount}, {expense.category}\n")
     
 
-def summarize_expenses():
+def summarize_expenses(expense_file_path):
     print(f"Summarizing User Expense")
  
 
 
-
 if __name__ == "__main__":
     main()
+
