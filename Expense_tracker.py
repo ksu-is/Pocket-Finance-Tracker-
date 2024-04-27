2d3114a11e01196c27c5e66f860b630730320f4f
Jsmi322 <Jsmi1467@students.kennesaw.edu>
2024-04-27 16:22:24 -0400

    Change to file
    I was having a hard time with making sure that the expenses that I was tracking were being transferred into  the file I created to keep all expenses in one place.  It wasn't recognizing the emojis at first but then I added the 'utf-8' that would help with unicode errors.

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -3,18 +3,20 @@ from Expense import Expense
 
 def main():
     print(f"Running Expense Tracker!")
+    expense_file_path = "expenses.csv"
 
 
     # Get user input for expense.
     expense = get_user_expense()
+    print(expense)
 
 
-    #Write their expense to a file.
-    save_expense_to_file()
+    # Write their expense to a file.
+    save_expense_to_file(expense, expense_file_path)
 
 
-    #Read file and summarize expenses.
-    summarize_expenses()
+    # Read file and summarize expenses.
+    summarize_expenses(expense_file_path)
   
 
 def get_user_expense():
@@ -35,10 +37,10 @@ def get_user_expense():
         for i, category_name in enumerate(expense_categories):
             print(f" {i + 1}. {category_name}")
 
-        value_range = f"[1 - {len (expense_categories)}]"
-        selected_index = int(input(f"Enter a categpry number {value_range}: ")) - 1
+        value_range = f"[1 - {len(expense_categories)}]"
+        selected_index = int(input(f"Enter a category number {value_range}: ")) - 1
 
-        if i in range(len(expense_categories)):
+        if selected_index in range(len(expense_categories)):
             selected_category = expense_categories[selected_index]
             new_expense = Expense(
                 name=expense_name, category=selected_category, amount=expense_amount
@@ -50,8 +52,10 @@ def get_user_expense():
 
 
 
-def save_expense_to_file(expense):
-    print(f"Saving User Expense: {expense}")
+def save_expense_to_file(expense: Expense, expense_file_path):
+    print(f"Saving User Expense: {expense} to {expense_file_path}")
+    with open(expense_file_path, 'a', encoding= 'utf-8') as f:
+        f.write(f"{expense.name}, {expense.amount}, {expense.category}\n")
     
 
 def summarize_expenses():
@@ -61,4 +65,3 @@ def summarize_expenses():
 
 if __name__ == "__main__":
     main()
-
---
+++ from Expense import Expense.py
@@ -0,0 +1,56 @@
+from Expense import Expense
+
+def main():
+    print(f"Running Expense Tracker!")
+    expense_file_path = "expenses.csv"
+
+    # Get user input for expense.
+    expense = get_user_expense()
+    print(expense)
+
+    # Write their expense to a file.
+    save_expense_to_file(expense, expense_file_path)
+
+    # Read file and summarize expenses.
+    summarize_expenses(expense_file_path)
+
+
+def get_user_expense():
+    print(f"Getting User Expense")
+    expense_name = input("Enter expense name: ")
+    expense_amount = float(input("Enter expense amount: "))
+    expense_categories = [
+        "🥞Food", 
+        "🏠Home", 
+        "💼Work", 
+        "🎊Fun", 
+        "✨Misc",
+        "🩺Health"
+    ]
+
+    while True:
+        print("Select a category: ")
+        for i, category_name in enumerate(expense_categories):
+            print(f" {i + 1}. {category_name}")
+
+        value_range = f"[1 - {len(expense_categories)}]"
+        selected_index = int(input(f"Enter a category number {value_range}: ")) - 1
+
+        if selected_index in range(len(expense_categories)): 
+            selected_category = expense_categories[selected_index]
+            new_expense = Expense(
+                name=expense_name, category=selected_category, amount=expense_amount
+            )
+            return new_expense
+        else:
+            print("Invalid Category. Please try again!")
+
+
+def save_expense_to_file(expense: Expense, expense_file_path):
+    print(f"Saving User Expense: {expense} to {expense_file_path}")
+    with open(expense_file_path, 'a') as f:
+        f.write(f"{expense.name}, {expense.amount}, {expense.category}\n")
+    
+
+def summarize_expenses(expense_file_path):
+    print(f"Summarizing User Expense")
