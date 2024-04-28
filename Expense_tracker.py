e8f8eb3c14926bbc4c7453c3ac06bd665a96a6cc
Jsmi322 <Jsmi1467@students.kennesaw.edu>
2024-04-27 23:59:56 -0400

    Completion
    I have concluded the project by now being able to run the entire app and it runs how it needs to.

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -1,21 +1,24 @@
 from Expense import Expense
+import calendar
+import datetime
 
 
 def main():
     print(f"Running Expense Tracker!")
     expense_file_path = "expenses.csv"
+    budget = 1500
 
 
     # Get user input for expense.
-    # expense = get_user_expense()
+    expense = get_user_expense()
 
 
     # Write their expense to a file.
-    # save_expense_to_file(expense, expense_file_path)
+    save_expense_to_file(expense, expense_file_path)
 
 
     # Read file and summarize expenses.
-    summarize_expenses(expense_file_path)
+    summarize_expenses(expense_file_path, budget)
   
 
 def get_user_expense():
@@ -57,7 +60,7 @@ def save_expense_to_file(expense: Expense, expense_file_path):
         f.write(f"{expense.name}, {expense.amount}, {expense.category}\n")
     
 
-def summarize_expenses(expense_file_path):
+def summarize_expenses(expense_file_path, budget):
     print(f"Summarizing User Expense")
     expenses: list[Expense] = []
     with open(expense_file_path, 'r', encoding= 'utf-8') as f:
@@ -80,6 +83,20 @@ def summarize_expenses(expense_file_path):
     print("Expenses By Category 📈:")
     for key, amount in amount_by_category.items():
         print(f"  {key}: ${amount:.2f}")
+
+    total_spent = sum([ex.amount for ex in expenses])
+    print(f"💵Total spent ${total_spent:.2f}")
+
+    remaining_budget = budget - total_spent 
+    print(f"✅Budget Remaining: ${remaining_budget:.2f}")
+
+    now = datetime.datetime.now()
+    days_in_month = calendar.monthrange(now.year, now.month)[1]
+    remaining_days = days_in_month - now.day
+
+    daily_budget = remaining_budget / remaining_days
+    print(f"👉Budget Per Day: ${daily_budget:.2f}")
+
 
 if __name__ == "__main__":
      main()
